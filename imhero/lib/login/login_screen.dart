ae83125e655c844f76fe10b8b730e7d176d30bfd
thsaudwls <39595042+thsaudwls@users.noreply.github.com>
2024-02-22 02:47:41 +0900

    ..

--- imhero/lib/login/login_screen.dart
+++ imhero/lib/login/login_screen.dart
@@ -8,6 +8,10 @@ import 'package:imhero/common/text_form_field.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:imhero/login/sign_up_screen.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:routemaster/routemaster.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -16,8 +20,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  String username = '';
-  String password = '';
+  // String username = '';
+  // String password = '';
+
+  final _key = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwdController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -65,21 +73,23 @@ class _LoginScreenState extends State<LoginScreen> {
                     padding: EdgeInsets.fromLTRB(
                         app_width * 0.1, 25, app_width * 0.1, 20),
                     child: Column(children: [
-                      LoginTextFormField(
-                        hintText: 'ID',
-                        autofocus: true,
-                        onChanged: (String value) {
-                          username = value;
-                        },
-                      ),
+                      // LoginTextFormField(
+                      //   hintText: 'ID',
+                      //   autofocus: true,
+                      //   onChanged: (String value) {
+                      //     username = value;
+                      //   },
+                      // ),
+                      emailInput(),
                       const SizedBox(height: 12),
-                      LoginTextFormField(
-                        hintText: 'PW',
-                        autofocus: false,
-                        onChanged: (String value) {
-                          username = value;
-                        },
-                      ),
+                      // LoginTextFormField(
+                      //   hintText: 'PW',
+                      //   autofocus: false,
+                      //   onChanged: (String value) {
+                      //     username = value;
+                      //   },
+                      // ),
+                      passwordInput(),
                       const SizedBox(height: 12),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -115,13 +125,37 @@ class _LoginScreenState extends State<LoginScreen> {
                             width: app_width * 0.05,
                           ),
                           TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const RootTab()));
-                              print('sign in button');
+                            onPressed: () async {
+                              if (_key.currentState!.validate()) {
+                                // 여기에 작성
+                                try {
+                                  UserCredential authResult = await FirebaseAuth
+                                      .instance
+                                      .signInWithEmailAndPassword(
+                                    email: _emailController.text,
+                                    password: _pwdController.text,
+                                  );
+
+                                  if (authResult.user != null) {
+                                    Routemaster.of(context).push('/');
+                                  }
+                                } on FirebaseAuthException catch (e) {
+                                  if (e.code == 'user-not-found') {
+                                    debugPrint('No user found for that email.');
+                                  } else if (e.code == 'wrong-password') {
+                                    debugPrint(
+                                        'Wrong password provided for that user.');
+                                  }
+                                }
+                              }
                             },
+                            // onPressed: () {
+                            //   Navigator.push(
+                            //       context,
+                            //       MaterialPageRoute(
+                            //           builder: (context) => const RootTab()));
+                            //   print('sign in button');
+                            // },
                             style: ButtonStyle(
                                 padding: MaterialStateProperty.all<EdgeInsets>(
                                     EdgeInsets.fromLTRB(app_width * 0.075, 10,
@@ -212,5 +246,115 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+
+  TextFormField emailInput() {
+    return TextFormField(
+      controller: _emailController,
+      autofocus: true,
+      validator: (val) {
+        if (val!.isEmpty) {
+          return 'The input is empty.';
+        } else {
+          return null;
+        }
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Input your email address.',
+        labelText: 'Email Address',
+        labelStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  TextFormField passwordInput() {
+    return TextFormField(
+      controller: _pwdController,
+      obscureText: true,
+      autofocus: true,
+      validator: (val) {
+        if (val!.isEmpty) {
+          return 'The input is empty.';
+        } else {
+          return null;
+        }
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Input your password.',
+        labelText: 'Password',
+        labelStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton loginButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        if (_key.currentState!.validate()) {
+          // 여기에 작성
+          try {
+            UserCredential authResult =
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+              email: _emailController.text,
+              password: _pwdController.text,
+            );
+
+            if (authResult.user != null) {
+              // 로그인 성공한 경우
+              // 해당 사용자의 정보를 Firestore에서 가져옴
+              // DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+              //     .collection('Users')
+              //     .doc(authResult.user!.uid)
+              //     .get();
+
+              // 사용자 정보를 Map 형태로 추출
+
+              // '/home' 라우트로 이동하면서 사용자 정보를 전달
+
+              Routemaster.of(context).push('/');
+            }
+          } on FirebaseAuthException catch (e) {
+            if (e.code == 'user-not-found') {
+              debugPrint('No user found for that email.');
+            } else if (e.code == 'wrong-password') {
+              debugPrint('Wrong password provided for that user.');
+            }
+          }
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: const Text(
+          "Login",
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> saveUserInfo(User user) async {
+    try {
+      // Firestore의 'users' 컬렉션에 사용자 정보 추가
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'email': user.email,
+        'displayName': user.displayName,
+        // 추가로 필요한 사용자 정보는 여기에 추가할 수 있습니다.
+      });
+
+      print("User info saved to Firestore");
+    } catch (e) {
+      print("Error saving user info: $e");
+    }
   }
 }
